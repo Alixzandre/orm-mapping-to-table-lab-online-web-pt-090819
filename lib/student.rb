cad0a793264965a8a96e28ab101a08970a763af4
Alexander Bispo <alexbispo173@gmail.com>
2019-11-17 00:07:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,16 @@
 class Student
+  
+    attr_accessor :name, :grade
+    attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+   def initialize(id = nil, name, grade)
+     @id = id 
+     @name = name
+     @grade = grade
+   end
+   
+   def self.create_table
+     
+   end
   
 end
