8d3438d99411ebb44850e75065b0cb652ecc1096
Alexander Bispo <alexbispo173@gmail.com>
2019-11-17 00:08:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,14 @@ class Student
    end
    
    def self.create_table
-     
-   end
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
   
 end
